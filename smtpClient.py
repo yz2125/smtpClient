e7c2fa11f3516ca6a731bc5b91cf7de37a614e2a
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-27 22:26:55 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -1,23 +1,23 @@
 from socket import *
-import base64
-#def smtp_client(port=1025, mailserver='127.0.0.1'):
+
+def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailserver = '127.0.0.1'
-    port = 1025
+    #mailserver = '127.0.0.1'
+    #port = 1025
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
-    clientSocket = socket(AF_INET,SOCK_STREAM)
-    clientSocket.connect(mailserver,port)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
     #print(recv) #You can use these print statement to validate return codes from the server.
     #if recv[:3] != '220':
-    #    print('220 reply not received from server.')
+        #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
@@ -29,14 +29,14 @@ import base64
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    fromCommand = '<yz2125@nyu.edu>\r\n'
+    fromCommand = "MAIL FROM: <yz2125@nyu.edu>\r\n"
     clientSocket.send(fromCommand.encode())
     recv2 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    rcptToCommand = '<zhangyuxin1169@gmail.com>\r\n'
+    rcptToCommand = "RCPT TO: <zhangyuxin1169@gmail.com>\r\n"
     clientSocket.send(rcptToCommand.encode())
     recv3 = clientSocket.recv(1024).decode()
     #print(recv3)
@@ -46,8 +46,6 @@ import base64
     # Fill in start
     data = 'DATA\r\n'
     clientSocket.send(data.encode())
-    #clientSocket.send(msg.encoded())
-    #clientSocket.send(endmsg.encode())
     recv4 = clientSocket.recv(1024).decode()
     # Fill in end
 
@@ -60,13 +58,13 @@ import base64
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
-    clientSocket.send(endmsg.endcode())
+    clientSocket.send(endmsg.encode())
     recv5 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
-    quitCommand = "QUIT\r\n"
+    quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
     recv6 = clientSocket.recv(1024).decode()
     clientSocket.close()
@@ -74,5 +72,5 @@ import base64
 
 
 if __name__ == '__main__':
-    #smtp_client(1025, '127.0.0.1')
-    main()
+    smtp_client(1025, '127.0.0.1')
+
