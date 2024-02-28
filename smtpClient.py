95da7fca1faa9e9a5146c380818926c52b24e48c
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-02-27 21:15:08 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -5,12 +5,13 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailserver = ('127.0.0.1',1250)
+    
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
     clientSocket = socket(AF_INET,SOCK_STREAM)
     clientSocket.connect((mailserver, port))
+    clientSocket.listen()
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
